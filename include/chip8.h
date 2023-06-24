e39bc1036808d9dfbf4ef2837693eb01906ab5d3
jyotiraditya.Samal <jyotiraditya.samal@gmail.com>
2023-06-25 04:14:03 +0530

    checked memory function in main

---
+++ include/chip8.h
@@ -0,0 +1,12 @@
+#ifndef CHIP8_H
+#define CHIP8_H
+
+#include "config.h"
+#include "chip8memory.h"
+
+struct chip8
+{
+    struct chip8memory MEMORY;
+};
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 #include "SDL2/SDL.h"
+#include "chip8.h"
 
 int main(int argc, char **argv)
 {
+    struct chip8 Chip8;
+    chip8_memory_set(&Chip8.MEMORY, 0x400, 'Z');
+    printf("%c\n", chip8_memory_get(&Chip8.MEMORY, 50));
+
     SDL_Init(SDL_INIT_EVERYTHING);
     SDL_Window *window = SDL_CreateWindow( // Creates a Window
-        "Chip8 Window",
+        EMULATOR_WINDOW_TITLE,
         SDL_WINDOWPOS_UNDEFINED,
         SDL_WINDOWPOS_UNDEFINED,
-        640, 320, SDL_WINDOW_SHOWN);
+        CHIP8_WIDTH * CHIP8_WINDOW_MULTIPLIER, CHIP8_HEIGHT * CHIP8_WINDOW_MULTIPLIER, SDL_WINDOW_SHOWN);
 
     SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_TEXTUREACCESS_TARGET);
     while (1)
